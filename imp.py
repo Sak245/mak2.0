847c44dd0bb431f4a9b3f5ee6e6654e0a2e1f7ca
Sak245 <aryalkatkar@gmail.com>
2024-09-04 20:28:28 +0530

    Update imp.py

--- imp.py
+++ imp.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pickle
